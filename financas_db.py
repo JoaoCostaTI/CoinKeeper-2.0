6aa93c67b6466ffdcadd70d58697e83541ac0cdd
JoaoCostaTI <joao.costapp@outlook.com>
2026-01-09 11:27:12 -0300

    as

--- financas_db.py
+++ financas_db.py
@@ -1,5 +1,24 @@
 import sqlite3
 
+from datetime import date, datetime
+
+# 1. FUNÇÕES AUXILIARES (Ficam SOLTAS, fora da classe)
+
+def adaptar_data(minha_data):
+    # Essa função recebe um objeto 'date' e deve retornar uma STRING (texto)
+    # Dica: use o método .isoformat() da data
+    return minha_data.isoformat() 
+
+def converter_data(dado_em_bytes):
+    # Essa função recebe bytes do banco e deve retornar um objeto 'date'
+    # Dica: use o datetime.strptime(dado_em_bytes.decode('utf-8'), '%Y-%m-%d').date()
+    # Copie a lógica que usamos no exercício anterior aqui:
+    return datetime.strptime(dado_em_bytes.decode('utf-8'), '%Y-%m-%d').date()
+
+# 2. REGISTRO (Avisar o SQLite que essas funções existem)
+sqlite3.register_adapter(date, adaptar_data)
+sqlite3.register_converter("DATE", converter_data)
+
 class BancoDeDados:
     def __init__(self, nome_banco):
         self.banco = nome_banco
@@ -12,19 +31,9 @@ class BancoDeDados:
         if self.conexao:
             self.conexao.close()
     
-    def criar_tabela(self):
-        self.conectar()
-        cursor = self.conexao.cursor()
+    def adaptar_data(self):
+        pass
 
-        cursor.execute('''
-                    create table if not exists teste(
-                       id integer primary key autoincrement,
-                       tipo text,
-                       categoria text
-                       )
-                    ''')
-        self.conexao.commit()
-        self.desconectar()
+    def converter_data(self):
+        pass
 
-db = BancoDeDados('financas.db')
-db.criar_tabela()
